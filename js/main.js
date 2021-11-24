e4b8263f0e683b8cdd1c556e17d9ca65d6260f0d
Eduardo Corona <eduardocorona@Eduardos-MacBook-Pro.local>
2021-11-23 16:08:12 -0800

    update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,4 @@
 /*----- constants -----*/
-//WAR RULES 
-//1. deck divided evenly, 26 each player cards are placed face down  
-//2. Players turn up cards at the same time, player with higher card takes both cards, places them faxce down at bottom of his deck
-//3.if cards same rank, war! each player turns up 1 card face down, one face up, player w higher card takes all 6
-//3 continued-- if in war, and both cards are same rank, both players place another card face down and another face up, player with higher card takes all 10 and so forth  
-//4. game ends when 1 player takes all cards  
-
-
-/*----- app's state (variables) -----*/
-
-
-/*----- cached element references -----*/
-
-
-/*----- event listeners -----*/
-
-
-/*----- functions -----*/
-
-// Constants
 const suits = ['s', 'c', 'd', 'h'];//spades clubs diamonds hearts
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 const cardLookup = {
@@ -27,28 +7,100 @@ const cardLookup = {
   "K": 13,
   "A": 14
 }
-const masterDeck = buildMasterDeck();
-function fillArray() {
-  let deck = [];
-  for (let i = 0; i < 52; i++)
-  deck[i] = i;
-  shuffle(deck);
-  splitCards
-}
 
+const masterDeck = buildMasterDeck();
+//war rules
+//1. deck divided evenly, 26 each player cards are placed face down  
+//2. Players turn up cards at the same time, player with higher card takes both cards, places them faxce down at bottom of his deck
+//3.if cards same rank, war! each player turns up 1 card face down, one face up, player w higher card takes all 6
+//3 continued-- if in war, and both cards are same rank, both players place another card face down and another face up, player with higher card takes all 10 and so forth  
+//4. game ends when 1 player takes all cards  
 
 /*----- app's state (variables) -----*/
-let shuffledDeck; // gives back 
+let shuffledDeck, pDeck, cDeck, pHand, cHand, cPoints, pPoints, winner;
+
+
+
+
+/*----- cached element references -----*/
+let message = document.getElementById("message")
+let playerScoreEl = document.getElementById("playerScore")
+let computerScoreEL = document.getElementById("computerScore")
+
+/*----- event listeners -----*/
+document.getElementById("draw-card").addEventListener("click", drawCard);
+
+/*----- functions -----*/
+init()
+
+
 
 function init() {//starts the whole thing 
-  shuffledDeck = getNewShuffledDeck(); // giving us array of cards, each card is obj (array of obj)
+  shuffledDeck = getNewShuffledDeck();
+  pDeck = shuffledDeck.splice(0, 26) //taking 26 card sout of deck
+  cDeck = shuffledDeck // only 26 cards left that are assigned to computer
+  pHand = []
+  cHand = []
+  pPoints = 0
+  cPoints = 0
+
 }
+
+function drawCard() {
+  let drawnComputer = cDeck.pop();
+  cHand.push(drawnComputer);
+  let drawnPlayer = pDeck.pop();
+  pHand.push(drawnPlayer);
+  document.getElementById("computer").className = `card ${drawnComputer.face}`;
+  document.getElementById("player").className = `card ${drawnPlayer.face}`;
+  compareCards(drawnComputer.value, drawnPlayer.value);
+playerWins();
+}
+function compareCards(x, y) {
+  if (x === y) {
+  message.innerHTML = "Tie!";
+  } else if (x > y) {
+    cPoints++;
+    computerScoreEL.innerHTML = cPoints;
+    message.innerHTML = "Computer wins!";
+
+  } else {
+    pPoints++;
+    playerScoreEl.innerHTML = pPoints;
+   message.innerHTML = "Player wins!";
+
+  }
+}
+
+
+function playerWins() {
+  if (pPoints === 10 && cPoints <= 10){
+  console.log("Player wins!");
+ } else if (cPoints === 10 || pPoints <= 10){
+  console.log("Computer wins!")
+
+  }else{
+    console.log("tie!")
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 function buildMasterDeck() { // starts array on l 34
   const deck = [];
   // Use nested forEach to generate card objects
-  suits.forEach(function(suit) { // iterates through each s c d h
-    ranks.forEach(function(rank) { // iterates through ranks j q k a line 36 37 will combne all cards i.e ranks + suits
+  suits.forEach(function (suit) { // iterates through each s c d h
+    ranks.forEach(function (rank) { // iterates through ranks j q k a line 36 37 will combne all cards i.e ranks + suits
       deck.push({ // still within ranks pushing each combo into deck array on line 34 
         // The 'face' property maps to the library's CSS classes for cards
         face: `${suit}${rank}`,
@@ -86,6 +138,6 @@ function getNewShuffledDeck() { //
 //   }if (i > 1 && i < 4)
 // }else {
   // container[i].className = `card back card-container`;
-}
+
 
 //create id's in html for cards that we can access through JS 
